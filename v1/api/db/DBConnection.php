2b0c6172f5d0e3d11348da0c98ea012fab229f79
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-28 12:30:52 +0530

    Create DBConnection

---
+++ v1/api/db/DBConnection.php
@@ -0,0 +1,28 @@
+<?php
+
+class DBConnection
+{
+
+    private $host = '127.0.0.1';
+    private $userName = "root";
+    private $password = "123";
+    private $database = "pciDB";
+    private $port = "3306";
+
+    private $connection;
+
+    /**
+     * DBConnection constructor.
+     */
+    public function __construct()
+    {
+        $this->connection = new Mysqli($this->host, $this->userName, $this->password, $this->database, $this->port);
+    }
+
+
+    //create a method to get the connection
+    public function getConnection()
+    {
+        return $this->connection;//return the connection
+    }
+}
