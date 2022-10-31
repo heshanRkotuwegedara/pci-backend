627c6cfc8253ca6f2857f5f75b007526f327e50b
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-31 10:17:28 +0530

    site GetAll function done

---
+++ v1/pci/admin/site/all/index.php
@@ -0,0 +1,21 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header("HTTP/1.1 200 OK");
+    die();
+}
+
+require_once __DIR__ . "./../../../../api/services/SiteService.php";
+
+$method = $_SERVER["REQUEST_METHOD"];
+$siteService = new SiteService();
+if ($method === "GET") {
+    $siteArray = $siteService->getAllSite();
+    echo json_encode($siteArray);
+}
